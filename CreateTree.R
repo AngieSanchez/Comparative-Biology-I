6cbbc67b9ed0d2e66a8262c925e7fe485d60f9fd
Angie Lizeth Sánchez Alvarez <angielizeths2@gmail.com>
2017-09-11 17:05:56 -0500

    Add files via upload
    How to create this tree?? ((A,B),((C,D),E))

---
+++ CreateTree.R
@@ -0,0 +1,19 @@
+######################### Exercises ###############
+
+# how to create this tree?? ((A,B),((C,D),E))
+
+edge1 <- c(7,7,9,9,8,8,6,6) ## Relations between internal nodes, terminal nodes.
+edge2 <- c(1,2,3,4,9,5,7,8)
+
+edge <- data.frame(edge1,edge2) ## concatenate the edges
+Nnode <- 4 ## Nnode, the number of internal nodes
+tip.label <- c("A","B","c","D","E") ##Names of terminal nodes.
+edge.length <- NULL ### Without length
+
+tree <- list(edge=edge, Nnode=Nnode, tip.label=tip.label, edge.length=edge.length) ##Create the list with all the objects.
+class(tree)<-"phylo" ## assign class
+## Error: We need collapse.singles(). Why? I don't know. :(
+str(tree) ##Show the structure of tree
+tree$edge
+plot(tree) ##Plot of tree
+#Voilà! I create a tree of zero.
