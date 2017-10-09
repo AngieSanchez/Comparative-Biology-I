561857e6063129daaf87a11fe17c8269c43c728d
Angie Lizeth Sánchez Alvarez <angielizeths2@gmail.com>
2017-10-09 17:09:02 -0500

    Add files via upload

---
+++ CreateTree.R
@@ -0,0 +1,22 @@
+######################### Exercises ###############
+
+# how to create this tree?? ((A,B),((C,D),E))
+
+edge1 <- c(7,7,9,9,8,8,6,6) ## Relations between internal nodes, terminal nodes.
+edge2 <- c(1,2,3,4,9,5,7,8)
+# Tips: Number the terminals and then start from the root
+#Edge1 or x= internal nodes, start from the root
+#Edge2 or y= terminal nodes, start from A.
+
+edge <- data.frame(edge1,edge2) ## concatenate the edges
+Nnode <- 4 ## Nnode, the number of internal nodes (n-1)
+tip.label <- c("A","B","c","D","E") ##Names of terminal nodes.
+edge.length <- NULL ### Without length
+
+tree <- list(edge=edge, Nnode=Nnode, tip.label=tip.label, edge.length=edge.length) ##Create the list with all the objects.
+class(tree)<-"phylo" ## assign class
+## Error: We need collapse.singles(). Why? I don't know. :(
+str(tree) ##Show the structure of tree
+tree$edge
+plot(tree) ##Plot of tree
+#Voilà! I create a tree of zero.
